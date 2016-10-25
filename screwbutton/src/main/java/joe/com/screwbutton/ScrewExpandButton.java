e2dd997c2fc81b2908316356c6dfdd181e78a946
Joe Chan <1030310877@qq.com>
2016-10-25 14:20:38 +0800

    添加消失动画

--- screwbutton/src/main/java/joe/com/screwbutton/ScrewExpandButton.java
+++ screwbutton/src/main/java/joe/com/screwbutton/ScrewExpandButton.java
@@ -12,6 +12,7 @@ import android.util.AttributeSet;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
+import android.view.animation.AccelerateInterpolator;
 import android.view.animation.DecelerateInterpolator;
 import android.widget.FrameLayout;
 
@@ -28,7 +29,9 @@ public class ScrewExpandButton extends CircleButton implements View.OnClickListe
     private boolean isExpand = false;
     private FrameLayout container;
     private ArrayList<ScrewAnimator> animators;
+    private ArrayList<ScrewDismissAnimator> dismissAnimators;
     private OnBtnClickListener btnClickListener;
+    private boolean touchOutsideToDismiss = true;
 
     public ScrewExpandButton(Context context) {
         this(context, null);
@@ -42,6 +45,7 @@ public class ScrewExpandButton extends CircleButton implements View.OnClickListe
         super(context, attrs, defStyleAttr);
         buttons = new ArrayList<>();
         animators = new ArrayList<>();
+        dismissAnimators = new ArrayList<>();
         if (context instanceof Activity) {
             rootLayout = (ViewGroup) ((Activity) context).getWindow().getDecorView().findViewById(android.R.id.content);
         }
@@ -87,25 +91,62 @@ public class ScrewExpandButton extends CircleButton implements View.OnClickListe
     @Override
     public void onClick(View v) {
         anchor = getAbsoluteXAndY();
+        collapseTimes = 0;
+        clearAnimators();
         if (!isExpand) {
-            animators.clear();
             expand();
-            isExpand = true;
         } else {
-            for (ScrewAnimator animator : animators) {
-                animator.cancelAnimator();
-            }
-            animators.clear();
             collapse();
-            isExpand = false;
         }
     }
 
+    private int collapseTimes = 0;
+
     public void collapse() {
-        if (rootLayout != null) {
-            rootLayout.removeView(container);
+        container.setOnClickListener(null);
+        for (int i = buttons.size() - 1; i >= 0; i--) {
+            CircleButton button = buttons.get(i);
+            ScrewDismissAnimator dismissAnimator = new ScrewDismissAnimator(button, i, (FrameLayout.LayoutParams) button.getLayoutParams());
+            dismissAnimator.start();
+            dismissAnimator.addListener(new Animator.AnimatorListener() {
+                @Override
+                public void onAnimationStart(Animator animation) {
+
+                }
+
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    collapseTimes++;
+                    if (collapseTimes == buttons.size()) {
+                        if (rootLayout != null) {
+                            rootLayout.removeView(container);
+                        }
+                        container.removeAllViews();
+                        isExpand = false;
+                        collapseTimes = 0;
+                    }
+                }
+
+                @Override
+                public void onAnimationCancel(Animator animation) {
+                    collapseTimes++;
+                    if (collapseTimes == buttons.size()) {
+                        if (rootLayout != null) {
+                            rootLayout.removeView(container);
+                        }
+                        container.removeAllViews();
+                        isExpand = false;
+                        collapseTimes = 0;
+                    }
+                }
+
+                @Override
+                public void onAnimationRepeat(Animator animation) {
+
+                }
+            });
+            dismissAnimators.add(dismissAnimator);
         }
-        container.removeAllViews();
     }
 
     int[] anchor;
@@ -117,6 +158,16 @@ public class ScrewExpandButton extends CircleButton implements View.OnClickListe
         if (rootLayout != null) {
             rootLayout.addView(container, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
         }
+        if (touchOutsideToDismiss) {
+            container.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    collapse();
+                }
+            });
+        } else {
+            container.setOnClickListener(null);
+        }
         for (int i = 0; i < buttons.size(); i++) {
             CircleButton btn = buttons.get(i);
             FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(subBtnSize, subBtnSize);
@@ -127,6 +178,7 @@ public class ScrewExpandButton extends CircleButton implements View.OnClickListe
             animator.start();
             animators.add(animator);
         }
+        isExpand = true;
     }
 
     private int[] getAbsoluteXAndY() {
@@ -148,6 +200,20 @@ public class ScrewExpandButton extends CircleButton implements View.OnClickListe
         return result;
     }
 
+    private void clearAnimators() {
+        for (ScrewAnimator animator : animators) {
+            animator.cancelAnimator();
+        }
+        for (ScrewDismissAnimator dismissAnimator : dismissAnimators) {
+            dismissAnimator.cancelAnimator();
+        }
+        animators.clear();
+        dismissAnimators.clear();
+    }
+
+    public void setTouchOutsideToDismiss(boolean touchOutsideToDismiss) {
+        this.touchOutsideToDismiss = touchOutsideToDismiss;
+    }
 
     public interface OnBtnClickListener {
         void onBtnClick(int btnId);
@@ -168,10 +234,10 @@ public class ScrewExpandButton extends CircleButton implements View.OnClickListe
 
         private void init() {
             ObjectAnimator alphaAnimator = ObjectAnimator.ofFloat(target, "alpha", 0f, 1f);
-            alphaAnimator.setDuration(300 + 150 * (index + 1));
+            alphaAnimator.setDuration(150 + 80 * (index + 1));
             ValueAnimator animator = ValueAnimator.ofFloat(0f, index / 7f);
             animator.setTarget(target);
-            animator.setDuration(300 + 150 * (index + 1));
+            animator.setDuration(150 + 80 * (index + 1));
             animator.setInterpolator(new DecelerateInterpolator());
             animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
                 @Override
@@ -182,18 +248,89 @@ public class ScrewExpandButton extends CircleButton implements View.OnClickListe
                     int y = (int) (r * Math.sin(Math.toRadians(t * 360)));
                     params.leftMargin = anchor[0] + x;
                     params.topMargin = anchor[1] + y;
-                    container.updateViewLayout(target, params);
+                    if (target.getParent() == container) {
+                        container.updateViewLayout(target, params);
+                    } else {
+                        animation.cancel();
+                    }
                 }
             });
             set = new AnimatorSet();
             set.play(alphaAnimator).with(animator);
-            set.start();
+        }
+
+        boolean isAnimating() {
+            if (set == null) {
+                return false;
+            }
+            return set.isRunning();
         }
 
         void start() {
             if (set != null) {
                 set.start();
             }
+        }
+
+        void cancelAnimator() {
+            if (set != null) {
+                set.cancel();
+            }
+        }
+    }
+
+    public class ScrewDismissAnimator {
+        private int index;
+        private FrameLayout.LayoutParams params;
+        private CircleButton target;
+        private AnimatorSet set;
+
+        ScrewDismissAnimator(CircleButton target, int index, FrameLayout.LayoutParams params) {
+            this.target = target;
+            this.index = index;
+            this.params = params;
+            init();
+        }
+
+        private void init() {
+            ObjectAnimator alphaAnimator = ObjectAnimator.ofFloat(target, "alpha", 1f, 0f);
+            alphaAnimator.setDuration(150 + 80 * (index + 1));
+            ValueAnimator animator = ValueAnimator.ofFloat(index / 7f, 0f);
+            animator.setTarget(target);
+            animator.setDuration(150 + 80 * (index + 1));
+            animator.setInterpolator(new AccelerateInterpolator());
+            animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                @Override
+                public void onAnimationUpdate(ValueAnimator animation) {
+                    float t = (float) animation.getAnimatedValue();
+                    float r = (getWidth() + 5) * (1 + t);
+                    int x = (int) (r * Math.cos(Math.toRadians(t * 360)));
+                    int y = (int) (r * Math.sin(Math.toRadians(t * 360)));
+                    params.leftMargin = anchor[0] + x;
+                    params.topMargin = anchor[1] + y;
+
+                    if (target.getParent() == container) {
+                        container.updateViewLayout(target, params);
+                    } else {
+                        animation.cancel();
+                    }
+                }
+            });
+            set = new AnimatorSet();
+            set.play(alphaAnimator).with(animator);
+        }
+
+        void start() {
+            if (set != null) {
+                set.start();
+            }
+        }
+
+        boolean isAnimating() {
+            if (set == null) {
+                return false;
+            }
+            return set.isRunning();
         }
 
         void addListener(Animator.AnimatorListener listener) {
